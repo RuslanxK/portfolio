eb4ed67c486e6d75c42f2a1a8e80560f96b71dc2
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-07-30 14:01:36 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -21,17 +21,17 @@ const App = () => {
     
        <section ref={myRef} className='projects'>
 
-        <h2 className='projects-headline'>PROJECTS</h2>
+        <h2 className='projects-headline'> TOP PROJECTS</h2>
         <ProjectsSection />
        </section>
 
        <section ref={myRef2} className='about-me'>
 
        <h2 className='about-headline'>ABOUT ME</h2>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac
-         turpis egestas. Sed faucibus turpis in eu mi bibendum neque. In massa tempor nec feugiat nisl pretium fusce id velit. Purus semper eget duis at tellus at urna condimentum mattis. <br /> <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac
-         turpis egestas. Sed faucibus turpis in eu mi bibendum neque. In massa tempor nec feugiat nisl pretium fusce id velit. Purus semper eget duis at tellus at urna condimentum mattis. <br /> <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac
-         turpis egestas. Sed faucibus turpis in eu mi bibendum neque. In massa tempor nec feugiat nisl pretium fusce id velit. Purus semper eget duis at tellus at urna condimentum mattis.</p>
+       <p>Hi, welcome to my portfolio! I am Ruslan Khomutov, a Full Stack Developer with expertise in creating robust and high-performance web applications. You can reach me at ruslanxkhomutov@gmail.com. <br /> <br />
+
+I have spent the past two years honing my skills in web development, working with technologies such as React.js, Next.js, Node.js, and MongoDB. At Click Digital Solutions, I developed several web applications implementing SSR and SEO best practices to improve site performance and user experience, and also I developed 25+ WordPress websites using Elementor, Crocoblock plugins. My role required collaborating with cross-functional teams and managing both frontend and backend tasks to deliver top-notch solutions. At XRhealth USA Company, I focused on enhancing website functionality and user experience using tools like Elementor, jQuery, and various API integrations.
+Throughout my career, I have developed a deep understanding of JavaScript frameworks, RESTful services, and cloud storage solutions like AWS S3. My key skills include problem-solving, project management, and effective communication, which have been instrumental in delivering successful projects and exceeding client expectations.</p>
 
        </section>
 
