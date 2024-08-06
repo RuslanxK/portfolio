87ce3e25236437e3a27c04f78d8d3b5ea2a3346f
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-08-06 19:54:58 +0300

    asdsd

--- src/App.js
+++ src/App.js
@@ -30,8 +30,9 @@ const App = () => {
        <h2 className='about-headline'>ABOUT ME</h2>
        <p>Hi, welcome to my portfolio! I am Ruslan Khomutov, a Full Stack Developer with expertise in creating robust and high-performance web applications. You can reach me at ruslanxkhomutov@gmail.com. <br /> <br />
 
-I have spent the past two years honing my skills in web development, working with technologies such as React.js, Next.js, Node.js, and MongoDB. At Click Digital Solutions, I developed several web applications implementing SSR and SEO best practices to improve site performance and user experience, and also I developed 25+ WordPress websites using Elementor, Crocoblock plugins. My role required collaborating with cross-functional teams and managing both frontend and backend tasks to deliver top-notch solutions. At XRhealth USA Company, I focused on enhancing website functionality and user experience using tools like Elementor, jQuery, and various API integrations.
-Throughout my career, I have developed a deep understanding of JavaScript frameworks, RESTful services, and cloud storage solutions like AWS S3. My key skills include problem-solving, project management, and effective communication, which have been instrumental in delivering successful projects and exceeding client expectations.</p>
+       With two years of experience in web development, I have honed my skills in technologies such as React.js, Next.js, Node.js, and MongoDB. At Click Digital Solutions, I developed several web applications, delivering scalable solutions using AWS, Redux, Socket.io, GraphQL, TypeScript, and Next.js for SSR. Additionally, I created over 25 WordPress websites using Elementor and Crocoblock plugins. My role involved collaborating with cross-functional teams and managing both frontend and backend tasks to deliver top-notch solutions.
+
+       My expertise extends to JavaScript frameworks, RESTful services, and cloud storage solutions like AWS S3. My key skills include problem-solving, project management, and effective communication, which have been instrumental in delivering successful projects and exceeding client expectations.</p>
 
        </section>
 
